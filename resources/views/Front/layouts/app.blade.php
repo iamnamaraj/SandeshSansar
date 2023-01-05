157d05c401aa80c61d346841e136853cb6292dec
Namaraj <namka_csit2074@lict.edu.np>
2023-01-05 19:09:10 +0545

    News Portal Frontend template mastering

---
+++ resources/views/Front/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <meta name="description" content="">
+        <title>SandeshSansar</title>
+
+        <link rel="icon" type="image/png" href="uploads/front_favicon.png">
+
+        @include('Front.layouts.styles')
+
+
+        @include('Front.layouts.scripts')
+
+        <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-6212352ed76fda0a"></script>
+
+        <!-- Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-84213520-6"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-84213520-6');
+        </script>
+
+    </head>
+    <body>
+        @include('Front.layouts.nav')
+
+        @yield('main_content')
+
+        @include('Front.layouts.footer')
+
+        @include('Front.layouts.footer_scripts')
+   </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AuthController;
+
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Front\AboutController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Front\HomeController as FrontHomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// ---------------------------  Frontend  ------------------------
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontHomeController::class, 'home'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
 
-Route::get('/admin/dashboard', [HomeController::class, 'index'])->name('admin.dashboard')->middleware('admin:admin');
+
+// --------------------- Backend ------------------------------
+
+
+// Admin Authentication
+
 Route::get('/admin/login', [AuthController::class, 'login'])->name('admin.login');
 Route::get('/admin/forget-password', [AuthController::class, 'forget_password'])->name('admin.forget-password');
 Route::post('/admin/login-submit', [AuthController::class, 'login_submit'])->name('admin.login-submit');
@@ -29,5 +27,9 @@ Route::post('/admin/forget-password/submit', [AuthController::class, 'forget_pas
 Route::get('/admin/reset-password/{token}/{email}', [AuthController::class, 'reset_password'])->name('admin.reset-password');
 Route::post('/admin/reset-password/submit', [AuthController::class, 'reset_password_submit'])->name('admin.reset-password.submit');
 
+//Admin Dashboard
+Route::get('/admin/dashboard', [HomeController::class, 'index'])->name('admin.dashboard')->middleware('admin:admin');
+
+//Admin Profile
 Route::get('/admin/profile', [ProfileController::class, 'edit_profile'])->name('admin.profile');
 Route::post('/admin/profile-submit', [ProfileController::class, 'edit_profile_submit'])->name('admin.profile.submit');
